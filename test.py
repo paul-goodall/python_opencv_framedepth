05a1916d1439f7b699a0b47958559ca6790ff875
paul-goodall <paul.thomas.goodall@gmail.com>
2022-12-19 07:08:45 +1100

    uploading the test movie with url x2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-movie_data="https://github.com/paul-goodall/python_opencv_framedepth/blob/main/test_movie.mov"
+movie_data="https://github.com/paul-goodall/python_opencv_framedepth/blob/main/test_movie.mov?raw=true"
 
 # print some useful information about a numpy array:
 def ndarray_info(nda):
